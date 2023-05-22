7721c10a5c646c5c2e0f78379f819522b2f72fa9
colorlessenergy <brianmunozdev@gmail.com>
2023-05-22 09:16:50 -0700

    add done functionality to todos

--- components/Todos.js
+++ components/Todos.js
@@ -1,12 +1,26 @@
 import { useTodosContext } from '@/contexts/TodosContext';
 
 const Todos = () => {
-    const { todos, currentList } = useTodosContext();
+    const { todos, setTodos, currentList } = useTodosContext();
 
-    return todos[currentList].todos.map(({ id, todo }) => {
+    const handleTodoChange = index => {
+        let cloneTodos = JSON.parse(JSON.stringify(todos));
+        cloneTodos[currentList].todos[index].done =
+            !cloneTodos[currentList].todos[index].done;
+
+        setTodos(cloneTodos);
+    };
+
+    return todos[currentList].todos.map(({ id, todo, done }, index) => {
         return (
             <div key={id} className="todo">
-                <input type="checkbox" id={`todo-${id}`} name={`todo-${id}`} />
+                <input
+                    type="checkbox"
+                    onChange={() => handleTodoChange(index)}
+                    id={`todo-${id}`}
+                    name={`todo-${id}`}
+                    checked={done}
+                />
                 <label
                     htmlFor={`todo-${id}`}
                     className="checkbox-input"></label>
